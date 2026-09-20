9170374c8ef3c70926853d77c79918111fa8ba10
Zoe BUCKZ <zoebuckz@Zoes-Mac-Pro.local>
2026-09-20 13:13:25 -0500

    Restore express import

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-import dotenv from "dotenv";
+import express from "express";import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
